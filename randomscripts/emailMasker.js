62d23a432cb1e4e3891974892d49901a52e522c9
CustardCup <daniel.c.chui@gmail.com>
2026-06-10 21:45:43 -0400

    Update emailMasker.js

--- randomscripts/emailMasker.js
+++ randomscripts/emailMasker.js
@@ -1,6 +1,9 @@
+// Takes an email address like johndoe@gmail.com and masks it as j*****e@gmail.com
+
 function maskEmail(email){
   const atPoint = email.indexOf("@");
   const censoredPart = email.slice(1,atPoint-1);
   const asterickPart = "*".repeat(atPoint-2);
   return email.replace(censoredPart,asterickPart);
 }
+
