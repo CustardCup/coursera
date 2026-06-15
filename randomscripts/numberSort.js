014cacc4268edef454c127fae9e004ad0478b029
CustardCup <daniel.c.chui@gmail.com>
2026-06-15 18:14:44 -0400

    Reorder operations in sortNumbers function

--- randomscripts/numberSort.js
+++ randomscripts/numberSort.js
@@ -3,7 +3,7 @@
 // Given a string of numbers separated by commas, return an array of the numbers sorted from smallest to largest.
 
 function sortNumbers(str) {
-  return str.split(",").sort((a , b) => a-b).map(Number);
+  return str.split(",").map(Number).sort((a , b) => a-b);
 };
 
 console.log(sortNumbers("0,6,-19,44,-2,7,0")) // [-19, -2, 0, 0, 6, 7, 44]
